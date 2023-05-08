7e218a15c92f12168d66763adbc8b7d766f1a4b8
devxsby <devxsby@gmail.com>
2023-05-08 17:31:51 +0900

    [Feat] Movie Detail, Movie Poster 엔티티 추가

---
+++ BoxOffice/Sources/Domain/Entity/MovieDetailEntity.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDetailEntity.swift
+//  BoxOffice
+//
+//  Created by devxsby on 2023/05/08.
+//
+
+import Foundation
+
+struct MovieDetailEntity {
+    let name: String
+    let directors: [String]
+    let productionYear: Int
+    let openingDate: Date
+    let showTime: Int
+    let watchGrade: String
+    let genres: [String]
+    let actors: [String]
+}
---
+++ BoxOffice/Sources/Domain/Entity/MoviePosterEntity.swift
@@ -0,0 +1,12 @@
+//
+//  MoviePosterEntity.swift
+//  BoxOffice
+//
+//  Created by devxsby on 2023/05/08.
+//
+
+import Foundation
+
+struct MoviePosterEntity {
+    let imageURL: String
+}
