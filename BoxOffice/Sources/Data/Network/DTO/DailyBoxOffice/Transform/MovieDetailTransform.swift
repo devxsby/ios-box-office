22dc782233e52e4257d7ceae9d964f784b37d5ee
Mason Kim <qwerty3345@naver.com>
2023-05-09 10:12:07 +0900

    [Feat] 영화상세정보 Entity 로 변환 로직 구현
    Response -> toDomain() 구현

---
+++ BoxOffice/Sources/Data/Network/DTO/DailyBoxOffice/Transform/MovieDetailTransform.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDetailTransform.swift
+//  BoxOffice
+//
+//  Created by Mason Kim on 2023/05/08.
+//
+
+import Foundation
+
+extension MovieInfo {
+    func toDomain() -> MovieDetailEntity {
+        return .init(name: name,
+                     directors: directors.map { $0.name },
+                     productionYear: Int(productionYear) ?? 0,
+                     openingDate: openingDate.convertToDate() ?? Date(),
+                     showTime: Int(showTime) ?? 0,
+                     watchGrade: audits.first?.watchGrade ?? "",
+                     genres: genres.map { $0.name },
+                     actors: actors.map { $0.name })
+    }
+}
+
+private extension String {
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        return formatter
+    }()
+    
+    func convertToDate() -> Date? {
+        Self.dateFormatter.date(from: self)
+    }
+}
